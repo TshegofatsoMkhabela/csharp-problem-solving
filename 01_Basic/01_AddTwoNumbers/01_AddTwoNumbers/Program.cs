47a4be2d19fe9e728ac371b758c5eb95766c1b6f
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-10-07 10:56:51 +0200

    feat: add 01_AddTwoNumbers solution

---
+++ 01_Basic/01_AddTwoNumbers/01_AddTwoNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double number1, number2;    // Input
+        double sum;                 // Output
+
+        // Get two valid numbers from the user
+        number1 = getValidNumber("Enter the first number: ");
+        number2 = getValidNumber("Enter the second number: ");
+
+        sum = number1 + number2;
+
+        // Display the result
+        Console.WriteLine($"The sum of your numbers is {sum}");
+    }
+
+    static double getValidNumber(string userPrompt)
+    {
+        double validNumber;
+
+        // Prompt the user until they enter a valid number
+        Console.Write(userPrompt);
+        while (!double.TryParse(Console.ReadLine(), out validNumber))
+        {
+            Console.Write("Invalid input. Please enter a valid number: ");
+        }
+
+        return validNumber;
+    }
+}
