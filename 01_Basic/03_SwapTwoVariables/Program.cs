2543ff02f50b1b1c61bfd37a7cdbc383c221033f
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-10-07 13:30:12 +0200

    feat: add 03_SwapTwoVariables solution

---
+++ 01_Basic/03_SwapTwoVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int variableA = 6;
+        int variableB = 7;
+
+        // Swap the values of variableA and variableB
+        (variableA, variableB) = (variableB, variableA);
+
+        Console.WriteLine($"Variable A is now: {variableA} and Variable B is now: {variableB}");
+    }
+}
