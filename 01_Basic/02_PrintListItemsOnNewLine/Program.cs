02f2871d1ea0443dba255c0a7c4e41b66a9808ec
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-10-07 12:59:30 +0200

    feat: add 02_PrintListItemsOnNewLine solution

---
+++ 01_Basic/02_PrintListItemsOnNewLine/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Define an array of Pokemon names 
+        string[] pokemons = {"Pikachu", "Bulbasaur", "Charmander", "Squirtle" };
+
+        // Print each Pokemon name on a new line
+        foreach (string pokemon in pokemons) 
+        {
+            Console.WriteLine(pokemon);
+        }
+    }
+}
