47e8e4c8b727f4b8ca5b04e48ad6a0a47455c4c8
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-10-11 07:32:15 +0200

    feat: add 217_ContainsDuplicate solution

---
+++ 04_LeetCode/217_ContainsDuplicate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Example use case
+        int[] nums = { 1, 2, 1, 2, 3, 4, 5 };
+        Console.WriteLine($"The status of whether the list contains duplicates is {ContainsDuplicate(nums)}");
+    }
+    static bool ContainsDuplicate(int[] nums)
+    {
+        // Convert array to list for easier manipulation
+        List<int> numsList = nums.ToList();
+
+        // Loop through the array, remove the current number from the list, and check if it still exists to detect duplicates.
+        // Stop one element before the end to avoid unnecessary checks
+        for (int i = 0; i<(nums.Length-1); i++)
+        {
+            int currentNum = nums[i];
+            numsList.Remove(currentNum);
+            if (numsList.Contains(currentNum))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
